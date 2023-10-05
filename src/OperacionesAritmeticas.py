db3063d6c4f3ae75040269ca35352aa2cb783126
Melany <71698104@continental.edu.pe>
2023-10-05 16:30:37 -0500

    se elimino todo

---
+++ src/OperacionesAritmeticas.py
@@ -0,0 +1,4 @@
+class OperacionesAritmeticas:
+
+    def suma(self, sumando1, sumando2):
+        return None
---
+++ tests/PruebaOperacionesAritmeticas.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class PruebaOperacionesAritmeticas(unittest.TestCase):
+    def test_suma_dosNumeros_respuestaSuma(self):
+        self.assertEqual(True, False)
+
