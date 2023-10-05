2a713f67b9cc97a15989a916a2f8160d71c3df45
Melany <71698104@continental.edu.pe>
2023-10-05 18:02:50 -0500

    prueba suma dos numeros

--- src/OperacionesAritmeticas.py
+++ src/OperacionesAritmeticas.py
@@ -1,4 +1,4 @@
 class OperacionesAritmeticas:
 
     def suma(self, sumando1, sumando2):
-        return None
+        return sumando1 + sumando2
--- tests/PruebaOperacionesAritmeticas.py
+++ tests/PruebaOperacionesAritmeticas.py
@@ -1,7 +1,18 @@
+import math
 import unittest
-
+from src.OperacionesAritmeticas import OperacionesAritmeticas
 
 class PruebaOperacionesAritmeticas(unittest.TestCase):
     def test_suma_dosNumeros_respuestaSuma(self):
-        self.assertEqual(True, False)
+        # Arrange
+        operacion = OperacionesAritmeticas()
+        sumando1 = 13.67
+        sumando2 = 15.20
+        resultadoEsperado = 28.87
 
+        # Do
+        resultadoActual = operacion.suma(sumando1, sumando2)
+
+        # Assert
+        diferencia = math.fabs(resultadoEsperado - resultadoActual)
+        self.assertLessEqual(diferencia,0.001)
